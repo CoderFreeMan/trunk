c3f008c869a594184efc47a8788e5ecbd9b766ca
CoderFreeMan <30991289+CoderFreeMan@users.noreply.github.com>
2018-09-16 23:09:08 +0800

    kafka-module

--- kafka-module/kafka-producer/src/main/java/com/ydj/MainApplication.java
+++ kafka-module/kafka-producer/src/main/java/com/ydj/MainApplication.java
@@ -3,6 +3,9 @@ package com.ydj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Hello world!
+ */
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
