af43397e172293b437719d9b55966b3835b4ee6f
yangdejun <yangdejun@dajiashequ.com>
2018-09-27 13:34:57 +0800

    basic-utils modify DateUtil

--- basic-utils/src/main/java/com/ydj/date/DateUtil.java
+++ basic-utils/src/main/java/com/ydj/date/DateUtil.java
@@ -1,7 +1,11 @@
 package com.ydj.date;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * <p> Date             :2017/10/9 </p>
@@ -43,25 +47,129 @@ public class DateUtil {
     /**
      * 年月日 DataFormat
      */
-    private static DateFormat dateFormat = null;
+    private static DateFormat defaultDateFormat = null;
 
     /**
      * 时分秒 DataFormat
      */
-    private static DateFormat timeFormat = null;
+    private static DateFormat defaultTimeFormat = null;
 
     /**
      * 年月日 时分秒 DataFormat
      */
-    private static DateFormat datetimeFormat = null;
+    private static DateFormat defaultDatetimeFormat = null;
 
     /**
      * 类加载时创建对象，防止在方法中创建多余的对象
      */
     static {
-        dateFormat = new SimpleDateFormat(DATE_DEFAULT_FORMAT);
-        timeFormat = new SimpleDateFormat(TIME_DEFAULT_FORMAT);
-        datetimeFormat = new SimpleDateFormat(DATETIME_DEFAULT_FORMAT);
+        defaultDateFormat = new SimpleDateFormat(DATE_DEFAULT_FORMAT);
+        defaultTimeFormat = new SimpleDateFormat(TIME_DEFAULT_FORMAT);
+        defaultDatetimeFormat = new SimpleDateFormat(DATETIME_DEFAULT_FORMAT);
+    }
+
+    public static final String NULL_OR_BLANK_EXCEPTION = "params must be not null or blank string";
+
+    public static final String NULL_EXCEPTION = "params must be not null";
+
+    /**
+     * 将"年-月-日" 格式的字符串转换为Date类型
+     * @param dateStr 需要转换的"年-月-日"格式的字符串
+     * @return 转换后的 Date 格式
+     * @throws ParseException
+     */
+    public static Date parseDefaultDate(String dateStr) throws ParseException {
+        if (StringUtils.isBlank(dateStr)) {
+            throw new NullPointerException(NULL_OR_BLANK_EXCEPTION);
+        }
+        return defaultDateFormat.parse(dateStr);
+    }
+
+    /**
+     * 将"时:分:秒"格式的字符串转换为Date类型，没有多少实际意义
+     * @param timeStr 需要转换的"时:分:秒"格式的字符串
+     * @return 转换后的 Date 格式, 年月日时 1970-01-01
+     * @throws ParseException
+     */
+    public static Date parseDefaultTime(String timeStr) throws ParseException {
+        if (StringUtils.isBlank(timeStr)) {
+            throw new NullPointerException(NULL_OR_BLANK_EXCEPTION);
+        }
+        return defaultTimeFormat.parse(timeStr);
+    }
+
+    /**
+     * 将"年-月-日 时:分:秒"格式的字符串转换为Date类型
+     * @param dateTimeStr 需要转换的"年-月-日 时:分:秒"格式的字符串
+     * @return 转换后的 Date 格式
+     * @throws ParseException
+     */
+    public static Date parseDefaultDateTime(String dateTimeStr) throws ParseException {
+        if (StringUtils.isBlank(dateTimeStr)) {
+            throw new NullPointerException(NULL_OR_BLANK_EXCEPTION);
+        }
+        return defaultDatetimeFormat.parse(dateTimeStr);
+    }
+
+    /**
+     * 将Date类型的时间转换为"年-月-日"类型的时间字符串
+     * @param date 需要转换的 Date
+     * @return 转换后的"年-月-日"格式的时间字符串
+     */
+    public static String parseDefaultDate(Date date) {
+        if (null == date) {
+            throw new NullPointerException(NULL_EXCEPTION);
+        }
+        return defaultDateFormat.format(date);
+    }
+
+    /**
+     * 将Date类型的时间转换为"时:分:秒"类型的时间字符串
+     * @param date 需要转换的 Date
+     * @return 转换后的"时:分:秒"格式的时间字符串
+     */
+    public static String parseDefaultTime(Date date) {
+        if (null == date) {
+            throw new NullPointerException(NULL_EXCEPTION);
+        }
+        return defaultTimeFormat.format(date);
+    }
+
+    /**
+     * 将Date类型的时间转换为"年-月-日 时:分:秒"类型的时间字符串
+     * @param date 需要转换的 Date
+     * @return 转换后的"年-月-日 时:分:秒"格式的时间字符串
+     */
+    public static String parseDefaultDateTime(Date date) {
+        if (null == date) {
+            throw new NullPointerException(NULL_EXCEPTION);
+        }
+        return defaultDatetimeFormat.format(date);
+    }
+
+    /**
+     * 将时间戳转换为 Date
+     * @param timestamp 需要转换为 Date 格式的时间戳
+     * @return 转换为Date
+     * @throws ParseException
+     */
+    public static Date parseTimestampToDate(Long timestamp) throws ParseException {
+        if (null == timestamp) {
+            throw new NullPointerException(NULL_EXCEPTION);
+        }
+        return defaultDatetimeFormat.parse(defaultDatetimeFormat.format(timestamp));
+    }
+
+    /**
+     * 将Date类型转换为时间戳类型
+     * @param date 需要转换为时间戳类型的Date
+     * @return 转换为时间戳后的值
+     */
+    public static long parseDateToTimestamp(Date date) {
+        if (null == date) {
+            throw new NullPointerException(NULL_EXCEPTION);
+        }
+        return date.getTime();
     }
 
 }
---
+++ basic-utils/src/test/java/com/ydj/date/DateUtilTest.java
@@ -0,0 +1,64 @@
+package com.ydj.date;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * <p> Date             :2017/10/9 </p>
+ * <p> Module           : </p>
+ * <p> Description      : </p>
+ * <p> Remark           : </p>
+ *
+ * @author yangdejun
+ * @version 1.0
+ * <p>--------------------------------------------------------------</p>
+ * <p>修改历史</p>
+ * <p>    序号    日期    修改人    修改原因    </p>
+ * <p>    1                                     </p>
+ */
+public class DateUtilTest {
+
+    @Test
+    public void getDefaultDate() throws ParseException {
+        System.out.println(DateUtil.parseDefaultDate("2018-9-27"));
+    }
+
+    @Test
+    public void getDefaultTime() throws ParseException {
+        System.out.println(DateUtil.parseDefaultTime("11:11:11"));
+    }
+
+    @Test
+    public void getDefaultDateTime() throws ParseException {
+        System.out.println(DateUtil.parseDefaultDateTime("2018-9-27 11:29:36"));
+    }
+
+    @Test
+    public void parseDefaultDate() {
+        System.out.println(DateUtil.parseDefaultDate(new Date()));
+    }
+
+    @Test
+    public void parseDefaultTime() {
+        System.out.println(DateUtil.parseDefaultTime(new Date()));
+    }
+
+    @Test
+    public void parseDefaultDateTime() {
+        System.out.println(DateUtil.parseDefaultDateTime(new Date()));
+    }
+
+    @Test
+    public void parseTimestampToDate() throws ParseException {
+        System.out.println(DateUtil.parseTimestampToDate(System.currentTimeMillis()));
+    }
+
+    @Test
+    public void parseDateToTimestamp() {
+        System.out.println(DateUtil.parseDateToTimestamp(new Date()));
+    }
+}
