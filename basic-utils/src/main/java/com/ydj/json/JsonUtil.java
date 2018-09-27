231cf3bd14270361df585b4a9a5add795627428b
yangdejun <yangdejun@dajiashequ.com>
2018-09-27 16:46:40 +0800

    basic-utils modify JsonUtil

--- basic-utils/src/main/java/com/ydj/json/JsonUtil.java
+++ basic-utils/src/main/java/com/ydj/json/JsonUtil.java
@@ -1,6 +1,9 @@
 package com.ydj.json;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Map;
 
 /**
  * <p> Date             :2017/10/9 </p>
@@ -33,6 +36,33 @@ public class JsonUtil {
      */
     public static <T> T jsonToBean(String json, Class<T> clazz) {
         return JSON.parseObject(json, clazz);
+    }
+
+    /**
+     * bean 转 json
+     * @param o 需要转为 json 的 bean
+     * @return 转换后的json对象
+     */
+    public static String beanToJson(Object o) {
+        return JSON.toJSONString(o, true);
+    }
+
+    /**
+     * json 转 Map
+     * @param json 需要转换为Map的json对象
+     * @return 转换后的Map对象
+     */
+    public static Map jsonToMap(String json) {
+        return JSONObject.parseObject(json);
+    }
+
+    /**
+     * map 转 json
+     * @param map 需要转为json对象的Map对象
+     * @return 转换后的json对象
+     */
+    public static String mapToJson(Map map) {
+        return JSONObject.parseObject(JSON.toJSONString(map)).toJSONString();
     }
 
 }
--- basic-utils/src/test/java/com/ydj/json/JsonUtilTest.java
+++ basic-utils/src/test/java/com/ydj/json/JsonUtilTest.java
@@ -1,10 +1,11 @@
 package com.ydj.json;
 
-import com.alibaba.fastjson.JSON;
-import com.ydj.testentity.xmlentity.User;
+import com.ydj.testentity.User;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * <p> Date             :2017/10/9 </p>
@@ -21,4 +22,37 @@ import static org.junit.Assert.*;
  */
 public class JsonUtilTest {
 
+    private static final String json = "{\"name\": \"ydj\"}";
+
+    @Test
+    public void jsonToBean() {
+        User user = JsonUtil.jsonToBean(json, User.class);
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void beanToJson() {
+        User user = new User();
+        user.setName("ydj");
+        String s = JsonUtil.beanToJson(user);
+        System.out.println(s);
+    }
+
+    @Test
+    public void jsonToMap() {
+        Map map = JsonUtil.jsonToMap(json);
+        map.forEach((k, v) -> {
+            System.out.println(k + "=" + v);
+            System.out.println(k.getClass());
+            System.out.println(v.getClass());
+        });
+    }
+
+    @Test
+    public void mapToJson() {
+        Map map = new HashMap();
+        map.put("name", "ydj");
+        String s = JsonUtil.mapToJson(map);
+        System.out.println(s);
+    }
 }
